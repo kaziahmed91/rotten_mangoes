7404ad030d77af1c1ec95f16d711b21701d7c217
Kazi Ahmed <kaziahmed91@gmail.com>
2016-06-21 12:47:33 -0400

    Basic Controllers and views
    Week_5 Day_1 push

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,35 @@
 class Movie < ActiveRecord::Base
+  has_many :reviews
+
+  validates :title,
+            presence: true
+
+  validates :director,
+            presence: true
+
+  validates :runtime_in_minutes,
+            numericality: { only_integer: true }
+
+  validates :description,
+            presence: true
+
+  validates :poster_image_url,
+            presence: true
+
+  validates :release_date,
+            presence: true
+
+  validate :release_date_is_in_the_past
+
+  def review_average
+    reviews.sum(:rating_out_of_ten) / reviews.size
+  end
+
+  protected
+
+  def release_date_is_in_the_past
+    if release_date.present?
+      errors.add(:release_date, 'should be in the past') if release_date > Date.today
+    end
+  end
 end
